c365af296d348635076d2ac9f8768e79a96b7ba4
chan <baguschandradp@gmail.com>
2024-02-06 11:21:33 +0800

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+50.times do
+  Product.create(name: Faker::Commerce.product_name, price: Faker::Commerce.price(range: 1000..100000))
+end
