0a45a6dc9d657d661d72a0622807143ccad4729e
Putu Chandra <baguschandradp@gmail.com>
2024-02-09 11:22:29 +0800

    add logout

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -4,20 +4,20 @@ class Cart < ApplicationRecord
   has_many :products, through: :cart_items
   has_one :order, dependent: :destroy
 
- # def create_order(user:, total:)
- #    order = Order.new(cart: self, user: user, total: total_amount, status: "created")
- #    cart_items.each do |cart_item|
- #      order_item = order.cart_item.build(product: cart_item.product, quantity: cart_item.quantity )
- #      order_item.order = order
- #    end
- #    order.save!
- #  end
+  # def create_order(user:, total:)
+  #    order = Order.new(cart: self, user: user, total: total_amount, status: "created")
+  #    cart_items.each do |cart_item|
+  #      order_item = order.cart_item.build(product: cart_item.product, quantity: cart_item.quantity )
+  #      order_item.order = order
+  #    end
+  #    order.save!
+  #  end
 
   def create_order(user:, total:)
-     Order.new(cart: self, user: user, total: total, status: "success")
+    Order.new(cart: self, user:, total:, status: 'success')
   end
 
   def total_amount
-     cart_items.sum { |item| item.product.price * item.quantity }
-   end
+    cart_items.sum { |item| item.product.price * item.quantity }
+  end
 end
